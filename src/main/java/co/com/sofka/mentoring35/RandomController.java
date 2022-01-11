30461f4981a5b43ffa61744becce06644914cedd
sebas <sebaslpez0519@gmail.com>
2022-01-11 15:19:59 -0500

    agregar el cross para todo

--- src/main/java/co/com/sofka/mentoring35/RandomController.java
+++ src/main/java/co/com/sofka/mentoring35/RandomController.java
@@ -18,7 +18,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 @RequestMapping(value = "/r")
 public class RandomController {
 
