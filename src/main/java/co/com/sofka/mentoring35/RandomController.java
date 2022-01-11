a4d9b69d424607d245f5f1510c1add1e53d0d3fc
sebas <sebaslpez0519@gmail.com>
2022-01-11 15:26:08 -0500

    son cross-origin

--- src/main/java/co/com/sofka/mentoring35/RandomController.java
+++ src/main/java/co/com/sofka/mentoring35/RandomController.java
@@ -18,7 +18,6 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-@CrossOrigin(origins = "*")
 @RequestMapping(value = "/r")
 public class RandomController {
 
